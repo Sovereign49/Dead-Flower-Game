839e60729ca1fe17894ded1fa4bd8a4aeb9345b3
Anakin <yahiahany21@gmail.com>
2022-08-11 23:58:03 +0200

    Anakin

--- test.gd
+++ test.gd
@@ -3,7 +3,7 @@ extends Spatial
 
 # Declare member variables here. Examples:
 # var a = 2
-var names = ["Bean"]
+var names = ["Bean", "Anakin"]
 
 
 # Called when the node enters the scene tree for the first time.
